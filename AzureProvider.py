f409d468c29f91cd11d9bb37ef3b382225d6cb6a
Ben Glick <glick@lclark.edu>
2019-06-14 12:47:50 -0500

    Add files via upload

--- AzureProvider.py
+++ AzureProvider.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import time
 from string import Template
 
 from parsl.dataflow.error import ConfigurationError
@@ -22,6 +21,8 @@ try:
 
     from msrestazure.azure_exceptions import CloudError
 
+    _api_enabled = True
+
 except ImportError:
     _api_enabled = False
 else:
@@ -41,11 +42,11 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
     """
     One of 2 methods are required to authenticate: keyfile, or environment
     variables. If  keyfile is not set, the following environment
-    variables must be set: `AZURE_CLIENT_ID` (the access key for your azure account),
+    variables must be set: `AZURE_CLIENT_ID` (the access key for
+    your azure account),
     `AZURE_CLIENT_SECRET` (the secret key for your azure account), and the
     `AZURE_TENANT_ID` (the session key for your azure account).
     """
-
 
     def __init__(self,
                  init_blocks=1,
@@ -57,7 +58,7 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
                  worker_init='',
                  instance_type_ref=None,
                  location='westus',
-                 group_name = 'parsl.auto',
+                 group_name='parsl.auto',
 
                  key_name=None,
                  key_file=None,
@@ -69,7 +70,8 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
                  linger=False,
                  launcher=SingleNodeLauncher()):
         if not _api_enabled:
-            raise OptionalModuleMissing(['azure'], "Azure Provider requires the azure-sdk-for-python module.")
+            raise OptionalModuleMissing(
+                ['azure'], "Azure Provider requires the azure-sdk-for-python module.")
 
         self._label = 'azure'
         self.init_blocks = init_blocks
@@ -94,10 +96,14 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
         self.resources = {}
         self.instances = []
 
-        env_specified = os.getenv("AZURE_CLIENT_ID") is not None and os.getenv("AZURE_CLIENT_SECRET") is not None
-        
+        env_specified = os.getenv("AZURE_CLIENT_ID") is not None and os.getenv(
+            "AZURE_CLIENT_SECRET") is not None
+
         if key_file is None and not env_specified:
-            raise ConfigurationError("Must specify either, 'key_file', or `AZURE_CLIENT_ID`, `AZURE_CLIENT_SECRET`, and `AZURE_TENANT_ID` environment variables.")
+            raise ConfigurationError(
+                "Must specify either, 'key_file', or\
+                 `AZURE_CLIENT_ID`, `AZURE_CLIENT_SECRET`,\
+                  and `AZURE_TENANT_ID` environment variables.")
 
         if key_file is not None:
             self.clientid = os.getenv("AZURE_CLIENT_ID")
@@ -112,33 +118,33 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
 
         self.get_clients()
 
-
     def get_clients(self):
         credentials, subscription_id = get_credentials()
-        self.resource_client = ResourceManagementClient(credentials, subscription_id)
-        self.compute_client = ComputeManagementClient(credentials, subscription_id)
-        self.network_client = NetworkManagementClient(credentials, subscription_id)
+        self.resource_client = ResourceManagementClient(
+            credentials, subscription_id)
+        self.compute_client = ComputeManagementClient(
+            credentials, subscription_id)
+        self.network_client = NetworkManagementClient(
+            credentials, subscription_id)
 
     def get_credentials(self):
-    subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
-    credentials = ServicePrincipalCredentials(
-        client_id=self.clientid,
-        secret=self.clientsecret,
-        tenant=self.tenantid
-    )
-    return credentials, subscription_id
+        subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+        credentials = ServicePrincipalCredentials(
+            client_id=self.clientid,
+            secret=self.clientsecret,
+            tenant=self.tenantid
+        )
+        return credentials, subscription_id
 
-
-    def submit(self, command='sleep 1', blocksize=1, tasks_per_node=1, job_name="parsl.auto"):
+    def submit(self, command='sleep 1', blocksize=1,
+               tasks_per_node=1, job_name="parsl.auto"):
         # Make sure group exists
-        self.resource_client.resource_groups.create_or_update(self.group_name, {'location': self.location})
-        self.resources.resources ["group"] = self.group_name
+        self.resource_client.resource_groups.create_or_update(
+            self.group_name, {'location': self.location})
+        self.resources.resources["group"] = self.group_name
 
         logger.info('\nCreating Linux Virtual Machine')
         nic = create_nic(network_client)
-
-
-
 
     def status(self, job_ids):
         pass
@@ -159,11 +165,9 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
         """Returns the current blocksize."""
         return len(self.instances)
 
-
     def create_nic(network_client):
         """Create a Network Interface for a VM.
         """
-        # Create VNet
         logger.info('\nCreating Vnet')
         async_vnet_creation = self.network_client.virtual_networks.create_or_update(
             self.group_name,
@@ -191,20 +195,19 @@ class AzureProvider(ExecutionProvider, RepresentationMixin):
         logger.info('\nCreate NIC')
         async_nic_creation = network_client.network_interfaces.create_or_update(
             self.group_name,
-             "{}.nic".format(self.group_name),
+            "{}.nic".format(self.group_name),
             {
                 'location': self.location,
                 'ip_configurations': [{
-                    'name':  "{}.ip.config".format(self.group_name),
+                    'name': "{}.ip.config".format(self.group_name),
                     'subnet': {
                         'id': subnet_info.id
                     }
                 }]
             }
         )
-        
-        return async_nic_creation.result()
 
+        return async_nic_creation.result()
 
 
 if __name__ == '__main__':
@@ -215,10 +218,7 @@ if __name__ == '__main__':
         'version': 'latest'
     }
 
-    provider = AzureProvider(key_file="azure_keys.json", instance_type_ref=vm_reference)
+    provider = AzureProvider(
+        key_file="azure_keys.json",
+        instance_type_ref=vm_reference)
     provider.submit()
-
-
-
-
-
